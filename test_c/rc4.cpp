604451bde992d71cab3f679cb24d4a3b20d70607
hp1909 <hoangphuc1909@live.com>
2018-03-14 00:14:49 +0900

    update file c

--- test_c/rc4.cpp
+++ test_c/rc4.cpp
@@ -3,23 +3,29 @@
 int main()
 {
     unsigned char key_length = 4;
-    unsigned char key[] = {64, 63, 62, 61};
+    unsigned char key[] = {97, 98, 99, 100};
     unsigned char sBox[256] = {0};
 
-    unsigned char i = 0; 
+    unsigned int i = 0; 
     for (i = 0; i <= 255; i++)
     {
         sBox[i] = i;
     }
 
     // KSA
-    unsigned char j = 0;
+    unsigned int j = 0;
     for (i = 0; i <= 255; i++) 
     {
-        j = (j + sBox[i] + key[i]) % 256;
+        j = (j + sBox[i] + key[i % key_length]) % 256;
         int temp = sBox[i];
         sBox[i] = sBox[j];
         sBox[j] = temp;
+        printf("%d\t", j);
+    }
+
+    printf("after KSA: \n");
+    for (int i = 0; i < 256; i++) {
+        printf("%d\t", sBox[i]);
     }
 
     // PRGA
